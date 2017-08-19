de3d1d4e1536d4be9dc5aacdff68e7bc6b535771
Levin丶枫过无痕 <1837307557@qq.com>
2017-08-19 14:20:16 +0800

    Liquibase数据库迁移代码

---
+++ battcn-boot-liquibase/src/main/java/com/battcn/Application.java
@@ -0,0 +1,15 @@
+package com.battcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Levin
+ * @date 2017-08-19.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
